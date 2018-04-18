39a5eeae97b605031d1f1a8be6a3ec97f7d13212
Beat Christen <beat.christen@systarch.com>
2018-04-18 17:02:39 +0200

    Make PATH available to all providers

--- lib/puppet/provider/mysql.rb
+++ lib/puppet/provider/mysql.rb
@@ -21,6 +21,8 @@ class Puppet::Provider::Mysql < Puppet::Provider
     '/opt/rh/rh-mariadb102/root/usr/lib64',
     '/opt/rh/rh-mariadb103/root/usr/lib',
     '/opt/rh/rh-mariadb103/root/usr/lib64',
+    '/opt/rh/rh-mariadb104/root/usr/lib',
+    '/opt/rh/rh-mariadb104/root/usr/lib64',
     '/opt/rh/mysql55/root/usr/lib',
     '/opt/rh/mysql55/root/usr/lib64',
     '/opt/rh/mariadb55/root/usr/lib',
